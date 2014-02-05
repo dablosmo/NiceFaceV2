ba41b3a655495f56098d7b750f3ba71b9bb8b4d4
David Zhang <zhangdt@usc.edu>
2014-02-05 02:02:23 -0800

    Added api key and secret

--- src/com/example/nicefacev2/StartGameActivity.java
+++ src/com/example/nicefacev2/StartGameActivity.java
@@ -1,10 +1,13 @@
 package com.example.nicefacev2;
 
+import client.Client;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
 public class StartGameActivity extends Activity {
+	
+	Client c = new Client("b06da00689e96bb8c8eecfcbbbbe4778", "PVWeWimndNqxMS5kHLXDvTAZzRdSUKbM"); 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
