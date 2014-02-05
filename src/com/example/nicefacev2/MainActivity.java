f1a1ff3d2ef8acb69f5683ea763ca542b250dedb
David Zhang <zhangdt@usc.edu>
2014-02-05 01:48:01 -0800

    Editted git ignore so I can push updated classpath and project files

--- src/com/example/nicefacev2/MainActivity.java
+++ src/com/example/nicefacev2/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.nicefacev2;
 
+import com.example.nicefacev2.MainActivity;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -17,6 +21,17 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void startGame(View view) { 
+		Intent mainIntent = new Intent(MainActivity.this, StartGameActivity.class); 
+		MainActivity.this.startActivity(mainIntent); 
+		MainActivity.this.finish();
+	}
+	
+	public void quitGame(View view) { 
+		finish(); 
+		System.exit(0);
 	}
 
 }
