9d3092690ae8c65482f61d75017246885abe9a0a
David Zhang <zhangdt@usc.edu>
2014-02-05 02:58:00 -0800

    First try to call api

--- src/com/example/nicefacev2/StartGameActivity.java
+++ src/com/example/nicefacev2/StartGameActivity.java
@@ -1,9 +1,20 @@
 package com.example.nicefacev2;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.http.ParseException;
+
 import client.Client;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class StartGameActivity extends Activity {
 	
@@ -13,6 +24,9 @@ public class StartGameActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start_game);
+		
+		Intent intent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);  
+		startActivityForResult(intent, 0); 
 	}
 
 	@Override
@@ -21,5 +35,47 @@ public class StartGameActivity extends Activity {
 		getMenuInflater().inflate(R.menu.start_game, menu);
 		return true;
 	}
+	
+	protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+	       super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+	       switch(requestCode) {
+	       case 0:
+	           if(resultCode == RESULT_OK){
+	           Uri selectedImage = imageReturnedIntent.getData();
+	                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+
+	                   Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+	                   cursor.moveToFirst();
+
+	                   int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+	                  //file path of captured image
+	                   String filePath = cursor.getString(columnIndex); 
+	                   //file path of captured image
+	                   File f = new File(filePath);
+	                   String filename= f.getName();
+	                   
+	                   try {
+	                	   c.detect(f);
+	                   } catch (ParseException e) {
+	                	   // TODO Auto-generated catch block
+	                	   e.printStackTrace();
+	                   } catch (IOException e) {
+	                	   // TODO Auto-generated catch block
+	                	   e.printStackTrace();
+	                   } 
+	                   
+	                   cursor.close();
+
+	                 //Convert file path into bitmap image using below line.
+	                  // yourSelectedImage = BitmapFactory.decodeFile(filePath);
+
+
+	                  //put bitmapimage in your imageview
+	                  //yourimgView.setImageBitmap(yourSelectedImage);
+	      }
+	       break;
+	   }
+	} 
 
 }
